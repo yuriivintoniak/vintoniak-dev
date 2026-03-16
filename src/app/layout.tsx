e0c648518e922be8422659a6c71f7f2ba2529109
yuriivintoniak <you@example.com>
2026-03-16 17:02:12 +0200

    feat: add footer with responsive copyright and credits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/Header/Header'
+import { Footer } from '@/components/Footer/Footer'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,6 +27,7 @@ export default function RootLayout({
         <main className='mb-auto p-4 flex-1 flex items-center justify-center'>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   )
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className='py-2 text-xl'>
+      <div className='flex flex-col md:flex-row items-center justify-between gap-5 md:gap-0 p-4'>
+        <div className='font-bold'>
+          <Link href='/'>LucaDCZ</Link>
+          <span className='text-text-accent text-2xl'>.</span>
+        </div>
+        <h6 className='text-center'>
+          Copyright {year} {" "}
+          <br className='inline 2xs:hidden' />
+          © Yurii Vintoniak
+        </h6>
+        <h6 className='text-center'>
+          Designed By {" "}
+          <br className='inline 2xs:hidden' />
+          <span className='text-text-accent'>Luca Da Corte</span>
+        </h6>
+      </div>
+    </footer>
+  )
+}
