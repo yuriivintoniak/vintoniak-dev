be11dc51428bda49e72102c583b7a0bbfe0f99b2
yuriivintoniak <you@example.com>
2026-03-21 15:15:39 +0200

    feat: add navigation to projects page in CTA button and refine button styles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section className='w-full flex flex-col-reverse justify-between items-center gap-10 lg:flex-row lg:gap-5'>
+    <section className='w-full pb-40 lg:pb-0 flex flex-col-reverse lg:flex-row items-center justify-between gap-10 lg:gap-5'>
       <div className='flex flex-col gap-5'>
         <div>
           <p className='mb-1 text-xl xs:text-2xl text-text-primary'>
@@ -22,12 +23,12 @@ export default function Home() {
           <button className='btn text-background bg-text-accent hover:bg-[#8A37F5]'>
             Contact me
           </button>
-          <button className='btn border hover:text-background hover:bg-foreground'>
+          <Link href='/projects' className='btn border hover:text-background hover:bg-foreground'>
             View my work
-          </button>
+          </Link>
         </div>
       </div>
-      <div className='inline-block p-0 xs:p-5 sm:p-10 rounded-full border border-text-accent/40'>
+      <div className='inline-block p-0 sm:p-10 rounded-full border border-text-accent/40'>
         <Image
           src='/avatar.jpg'
           width={380}
