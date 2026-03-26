fe1f995a66f0f5c0f87859c866f70efaac163bcd
yuriivintoniak <you@example.com>
2026-03-26 10:15:59 +0200

    chore: configure next.config for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/vintoniak-dev",
   devIndicators: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
