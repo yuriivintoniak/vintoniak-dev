dcca3d5e78a3e1e5b1521a07b51b40f2088cbdf0
yuriivintoniak <you@example.com>
2026-03-27 13:10:44 +0200

    feat: add about page and configure basePath for production

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <section className='w-full py-20'>
+      <h2 className='text-[30px] 2xs:text-[44px] sm:text-[56px] tracking-[-1px] font-bold'>
+        About me <span className='text-text-accent'>.</span>
+      </h2>
+    </section>
+  )
+}
