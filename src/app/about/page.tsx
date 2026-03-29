66e843c6443e843663ba6dbffb751861eeb65239
yuriivintoniak <you@example.com>
2026-03-29 11:25:12 +0300

    feat: add Globe component to AboutHero section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
+import { AboutHero } from '@/components/About/AboutHero'
+
 export default function About() {
   return (
     <section className='w-full py-20'>
-      <h2 className='text-[30px] 2xs:text-[44px] sm:text-[56px] tracking-[-1px] font-bold'>
-        About me <span className='text-text-accent'>.</span>
-      </h2>
+      <AboutHero />
     </section>
   )
 }
