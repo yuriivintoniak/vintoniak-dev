10fb73f2ebe5b2e2acfca192e213826499c41582
yuriivintoniak <you@example.com>
2026-03-31 14:27:42 +0300

    feat: enhance AboutHero typography and add stack separators

--- src/components/About/AboutHero/AboutHero.tsx
+++ src/components/About/AboutHero/AboutHero.tsx
@@ -1,19 +1,20 @@
 import { Globe } from './Globe'
 import { stackData } from './stackData'
+import { StarSeparator } from './StarSeparator'
 
 export function AboutHero() {
   return (
     <div>
-      <div className='flex flex-col gap-5'>
-        <h2 className='text-[30px] 2xs:text-[44px] sm:text-[56px] tracking-[-1px] font-bold'>
+      <div className='flex flex-col gap-3 pb-20'>
+        <h1 className='text-[48px] 2xs:text-[72px] sm:text-[90px] tracking-[-1px] font-bold'>
           About me<span className='text-text-accent'>.</span>
-        </h2>
-        <p className='2xs:text-xl sm:text-2xl text-text-secondary border-l-8 border-text-accent pl-4'>
+        </h1>
+        <p className='2xs:text-xl sm:text-2xl text-text-secondary border-l-8 border-text-accent pl-4 tracking-[0.5px]'>
           Developing beautiful and functional websites is what I love doing,
           and that's why I give my all in every new challenge.
         </p>
       </div>
-      <div className='flex justify-between pt-16'>
+      <div className='flex justify-between'>
         <div>
           <h2 className='text-[22px] font-bold mb-4'>
             My Stack<span className='text-text-accent'>.</span>
@@ -27,12 +28,12 @@ export function AboutHero() {
                     className={`flex ${category.title === 'Backend' ? 'animate-marquee-right' : 'animate-marquee-left'}`}
                   >
                     {category.items.map((item, j) => (
-                      <span
-                        key={j}
-                        className='mx-2 py-3 px-7 text-[#e6e6e6] bg-[#1a1a1a] rounded-xl whitespace-nowrap'
-                      >
-                        {item}
-                      </span>
+                      <div key={j} className='flex items-center'>
+                        <span className='mx-3 py-2 px-6 text-[#e6e6e6] bg-[#1a1a1a] rounded-xl whitespace-nowrap'>
+                          {item}
+                        </span>
+                        <StarSeparator />
+                      </div>
                     ))}
                   </div>
                 ))}
