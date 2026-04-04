6359c20e9f8ca5e4558301c5afb3375eae5b90e8
yuriivintoniak <you@example.com>
2026-04-04 13:25:02 +0300

    feat: add MyStory section and make it responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
+import { MyStory } from '@/components/About/MyStory/MyStory'
 import { AboutHero } from '@/components/About/AboutHero/AboutHero'
 
 export default function About() {
   return (
-    <section className='w-full py-20'>
-      <AboutHero />
+    <section className='w-full'>
+      <div className='pt-20 min-h-[calc(100vh-80px)]'>
+        <AboutHero />
+      </div>
+      <div className='min-h-screen pt-10 lg:pt-0 flex items-center'>
+        <MyStory />
+      </div>
     </section>
   )
 }
