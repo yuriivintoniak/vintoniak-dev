7a0b6cb610bb9b5a9d967e9dbcd584e6e448c4eb
yuriivintoniak <you@example.com>
2026-04-14 09:33:39 +0300

    refactor: extract social links into separate component

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import { ICONS, SOCIALS } from './constants'
+import { ContactLinks } from './ContactsLinks'
 
 export function Contacts() {
   return (
@@ -11,32 +11,7 @@ export function Contacts() {
           {'{'}
         </span>
       </div>
-      <div className='pl-8'>
-        <h3 className='text-4xl text-text-accent'>
-          I am open for job opportunities.
-        </h3>
-        <p className='text-2xl text-text-primary pt-2'>
-          My inbox is always open for collaboration or interviews.
-        </p>
-        <div className='flex flex-col gap-4 mt-16 text-2xl text-text-primary'>
-          {SOCIALS.map((social) => {
-            const Icon = ICONS[social.icon]
-
-            return (
-              <a
-                key={social.name}
-                href={social.url}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='flex items-center gap-3'
-              >
-                <Icon />
-                <span>{social.nickname}</span>
-              </a>
-            )
-          })}
-        </div>
-      </div>
+      <ContactLinks />
       <div className='mt-5'>
         <span className='sm:text-[56px] text-text-accent'>
           {'}'}
